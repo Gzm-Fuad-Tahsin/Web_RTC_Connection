ad4046ac0a3a4fae339e371d2b598af72b1648e1
Tahsin <gazitahsin323@gmail.com>
2025-07-01 14:37:13 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  console.log('New client connected:', socket.id);
+
+  socket.on('join', (roomId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit('user-joined', socket.id);
+  });
+
+  socket.on('offer', (data) => {
+    socket.to(data.room).emit('offer', data);
+  });
+
+  socket.on('answer', (data) => {
+    socket.to(data.room).emit('answer', data);
+  });
+
+  socket.on('ice-candidate', (data) => {
+    socket.to(data.room).emit('ice-candidate', data.candidate);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
